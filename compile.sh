a0d6ae92a66c580a439b98e5d1d417a5d85e388a
Brett Kuskie <fullaxx@gmail.com>
2021-02-08 14:46:38 -0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+GCCFLAGS=`libgcrypt-config --cflags`
+GCLIBS=`libgcrypt-config --libs`
+
 # -DNO_POUCH_RAND_START
-CFLAGS="-Wall"
+CFLAGS="-Wall ${GCCFLAGS}"
 OPTCFLAGS="${CFLAGS} -O2"
 DBGCFLAGS="${CFLAGS} -ggdb3"
 
@@ -16,41 +19,41 @@ gcc -Wall -ggdb3 incr_time.c -o incr_time.dbg
 gcc -Wall -ggdb3 main.c -o main.dbg -lpthread
 
 gcc ${OPTCFLAGS} dh.c pouch.c pot?.c getopts.c my_gcry_help.c \
--lpthread `libgcrypt-config --cflags --libs` -o dh.exe
+-lpthread ${GCLIBS} -o dh.exe
 
 gcc ${DBGCFLAGS} dh.c pouch.c pot?.c getopts.c my_gcry_help.c \
--lpthread `libgcrypt-config --cflags --libs` -o dh.dbg
+-lpthread ${GCLIBS} -o dh.dbg
 
 gcc ${OPTCFLAGS} -DLARGEPOUCH \
 dh.c pouch.c pot?.c getopts.c my_gcry_help.c \
--lpthread `libgcrypt-config --cflags --libs` -o dh_lp.exe
+-lpthread ${GCLIBS} -o dh_lp.exe
 
 gcc ${DBGCFLAGS} -DLARGEPOUCH \
 dh.c pouch.c pot?.c getopts.c my_gcry_help.c \
--lpthread `libgcrypt-config --cflags --libs` -o dh_lp.dbg
+-lpthread ${GCLIBS} -o dh_lp.dbg
 
 gcc ${OPTCFLAGS} -DSTATISTICS \
 dh.c pouch.c pot?.c getopts.c my_gcry_help.c \
--lpthread `libgcrypt-config --cflags --libs` -o stats.exe
+-lpthread ${GCLIBS} -o stats.exe
 
 gcc ${DBGCFLAGS} -DSTATISTICS \
 dh.c pouch.c pot?.c getopts.c my_gcry_help.c \
--lpthread `libgcrypt-config --cflags --libs` -o stats.dbg
+-lpthread ${GCLIBS} -o stats.dbg
 
 gcc ${OPTCFLAGS} -DSTATISTICS -DLARGEPOUCH \
 dh.c pouch.c pot?.c getopts.c my_gcry_help.c \
--lpthread `libgcrypt-config --cflags --libs` -o stats_lp.exe
+-lpthread ${GCLIBS} -o stats_lp.exe
 
 gcc ${DBGCFLAGS} -DSTATISTICS -DLARGEPOUCH \
 dh.c pouch.c pot?.c getopts.c my_gcry_help.c \
--lpthread `libgcrypt-config --cflags --libs` -o stats_lp.dbg
+-lpthread ${GCLIBS} -o stats_lp.dbg
 
 gcc ${OPTCFLAGS} \
 keygen.c pouch.c pot?.c getopts.c my_gcry_help.c \
--lpthread `libgcrypt-config --cflags --libs` -o keygen.exe
+-lpthread ${GCLIBS} -o keygen.exe
 
 gcc ${DBGCFLAGS} \
 keygen.c pouch.c pot?.c getopts.c my_gcry_help.c \
--lpthread `libgcrypt-config --cflags --libs` -o keygen.dbg
+-lpthread ${GCLIBS} -o keygen.dbg
 
 strip *.exe
